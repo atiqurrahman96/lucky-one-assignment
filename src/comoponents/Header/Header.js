e9c70f6991cc19ae145f18703694914fb8079c27
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-03-25 23:05:16 +0600

    header added

---
+++ src/comoponents/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <nav className='nav-container'>
+            <h1>Welcome To MY Special Cars Mart</h1>
+            <h2>We are Awaiting for You,Because You are Special</h2>
+        </nav>
+    );
+};
+
+export default Header;
